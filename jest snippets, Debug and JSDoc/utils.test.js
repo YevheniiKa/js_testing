00a9dbde05e64d06cbc536e2bee6eee9dde21852
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-04 19:56:51 +0200

    good test names

--- jest snippets, Debug and JSDoc/utils.test.js
+++ jest snippets, Debug and JSDoc/utils.test.js
@@ -1,15 +1,23 @@
 const { slice } = require("./utils");
 
-describe("slice", () => {
-  it("should return part of input", () => {
-    const result = slice("0123456789", 3, 8);
+describe(`'slice' function`, () => {
+  it(
+    `should return chars between 'begin' and not including 'end'` +
+      `if 0 < begin < end < input.length`,
+    () => {
+      const result = slice("0123456789", 3, 8);
 
-    expect(result).toBe("34567");
-  });
+      expect(result).toBe("34567");
+    },
+  );
 
-  it("should return from start index till the end", () => {
-    const result = slice("0123456789", 3);
+  it(
+    `should return chars between 'begin' and till the 'end' of input` +
+      `if 0 < begin < input.length and 'end' is omitted`,
+    () => {
+      const result = slice("0123456789", 3);
 
-    expect(result).toBe("3456789");
-  });
+      expect(result).toBe("3456789");
+    },
+  );
 });
