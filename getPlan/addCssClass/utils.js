147d590959550720f874fa6095b95dd9fe2f6418
Yevhenii Kaliaiev <eugenyr34@gmail.com>
2026-02-04 19:02:20 +0200

    Write a test before fixing a bug

--- getPlan/addCssClass/utils.js
+++ getPlan/addCssClass/utils.js
@@ -1,12 +1,14 @@
 function addCssClass(element, classToAdd) {
+  element.classname = element.classname.trim().replace(/ +/g, " ");
   if (element.classname.split(" ").includes(classToAdd)) {
     return;
   }
+
   element.classname += ` ${classToAdd}`;
 }
 
 const el = {
-  classname: "joke new",
+  classname: "     joke     new   ", // 'joke new'
 };
 
 addCssClass(el, "active");
--- getPlan/addCssClass/utils.test.js
+++ getPlan/addCssClass/utils.test.js
@@ -17,11 +17,39 @@ test("should check already existed class name", () => {
 
   expect(el.classname).toBe("joke new");
 });
-test("should check already existed class name", () => {
+
+test("should check already existed class name in class name", () => {
   const el = {
     classname: "joke new",
   };
   addCssClass(el, "ok");
 
   expect(el.classname).toBe("joke new ok");
+});
+
+test("should trim spaces", () => {
+  const el = {
+    classname: "     joke new     ",
+  };
+  addCssClass(el, "active");
+
+  expect(el.classname).toBe("joke new active");
+});
+
+test("should remove inner spaces", () => {
+  const el = {
+    classname: "     joke     new     ",
+  };
+  addCssClass(el, "active");
+
+  expect(el.classname).toBe("joke new active");
+});
+
+test("should remove all spaces for duplicate class name", () => {
+  const el = {
+    classname: "     joke     new     ",
+  };
+  addCssClass(el, "new");
+
+  expect(el.classname).toBe("joke new");
 });
